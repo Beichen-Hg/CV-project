3be0114e1a82d9f4ba2b0adbc1e9207b7a557515
JulianShi <130142228+Soif-de-connaissances@users.noreply.github.com>
2024-11-14 00:54:24 +0800

    Delete cv_group_assignment directory

--- cv_group_assignment/model.py
+++
@@ -1,9 +0,0 @@
-#模型定义模块
-import torch.nn as nn
-import torchvision.models as models
-
-def get_model(num_classes):
-    resnet50 = models.resnet50(pretrained=True)
-    num_ftrs = resnet50.fc.in_features
-    resnet50.fc = nn.Linear(num_ftrs, 10)
-    return resnet50
